328fbc99e5d2a0336bfa39973330cb6ca97d6200
Anna Vaganova <vaganova.ann00@gmail.com>
2020-11-14 11:47:01 +0700

    bad 3.2

--- lab3/L3.clj
+++ lab3/L3.clj
@@ -37,19 +37,34 @@
 (time (doall (parallel_filter_list `(1 -2 -3 -4 5 -6 7 -8 9))))
 (time (doall (parallel_filter `(1 -2 -3 -4 5 -6 7 -8 9))))
 
-; 3.2
+; 3.2 
+; (╯ ° □ °) ╯ (┻━┻)
+; плохо работает
 
 (defn my_lazy_partition
   ([m seq] (my_lazy_partition (drop m seq) m (take m seq)))
   ([seq m result]
    (lazy-seq (cons result (my_lazy_partition (drop m seq) m (take m seq))))))
 
+(defn future_batch
+  [batch]
+  (->> batch
+       (map #(future (doall (filter heavy_filter %))))
+       (doall)
+       (mapcat deref)
+       ))
+
 (defn lazy_parallel_filter
   [seq len_chunk len_batch]
   (->> seq
        (my_lazy_partition len_chunk)
        (my_lazy_partition len_batch)
-       
-       ; (╯ ° □ °) ╯ (┻━┻)
-       
+       (map #(future_batch %))
+       (mapcat identity)
        ))
+
+; тут работает
+(time (doall (take 3 (lazy_parallel_filter `(-1 2 -3 -4 -5 6 -7 8 -9) 2 2))))
+
+; тут не работает
+(time (doall (take 3 (lazy_parallel_filter (take 10 (range)) 2 2))))
